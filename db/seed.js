e732d8347cd2b0e011520420b4de7b83e1ce0f1c
jasarqui <jzarquilita@up.edu.ph>
2018-07-10 13:15:01 +0800

    Removed seed data

--- db/seed.js
+++ db/seed.js
@@ -1,31 +1,7 @@
 const db = require('../db');
-const sequelize = require('sequelize');
-const bcrypt = require('bcrypt');
 
-/* this is to hash the admin password */
-const saltRounds = 10;
-var adminPass = 'admin';
-bcrypt.hash('admin', saltRounds, (err, hash) => {
-  adminPass = hash;
-});
-
-/* this function seeds the users table */
-const seedUsers = () =>
-  db.Promise.map(
-    [
-      {
-        email: 'admin@irri.org',
-        password: adminPass,
-        firstname: 'admin',
-        lastname: 'istrator'
-      }
-    ],
-    user => db.model('users').create(user)
-  );
-
+/* this will restart our database */
 db.didSync
   .then(() => db.sync({ force: true }))
-  .then(seedUsers)
-  .then(user => console.log(`Seeded ${user.length} users`))
   .catch(error => console.error(error))
   .finally(() => db.close());
